d607ccbe4f0d76ff9538ba77edc878455debe66c
cjgarcia12 <christian.jgarcia03@gmail.com>
2024-07-26 17:15:12 -0400

    init commit

--- src/main/java/org/example/BankAccount.java
+++ src/main/java/org/example/BankAccount.java
@@ -1,2 +1,86 @@
-package org.example;public class BankAccount {
+package org.example;
+
+import java.util.Scanner;
+
+public class BankAccount {
+    private double balance;
+    private String accountHolderName;
+    private int accountNumber;
+
+    public BankAccount(double balance, String accountHolderName, int accountNumber) {
+        this.balance = balance;
+        this.accountHolderName = accountHolderName;
+        this.accountNumber = accountNumber;
+    }
+
+    public BankAccount() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Are you an existing customer? (0 to exit)\n1. Yes\n2. No");
+        int choice = scanner.nextInt();
+        if (choice == 1) {
+            System.out.println("Please enter your name: ");
+            this.accountHolderName = scanner.next();
+        } else if (choice == 2) {
+            System.out.println("Let's make a new account!\nWhat is the name for the account?");
+            this.accountHolderName = scanner.next();
+            System.out.println("What is the beginning balance for the account?");
+            this.balance = scanner.nextDouble();
+            this.accountNumber = (int) System.currentTimeMillis();
+
+        } else {
+            System.out.println("Goodbye!");
+        }
+
+    }
+
+    public void bankTransfer(BankAccount account, double amount) {
+        this.balance -= amount;
+        account.deposit(amount);
+
+    }
+
+    public double getBalance() {
+        return balance;
+    }
+
+    public void setBalance(double balance) {
+        this.balance = balance;
+    }
+
+    public String getAccountHolderName() {
+        return accountHolderName;
+    }
+
+    public void setAccountHolderName(String accountHolderName) {
+        this.accountHolderName = accountHolderName;
+    }
+
+    public int getAccountNumber() {
+        return accountNumber;
+    }
+
+    public void setAccountNumber(int accountNumber) {
+        this.accountNumber = accountNumber;
+    }
+
+    public void deposit(double amount) {
+        this.balance += amount;
+    }
+
+    public void withdrawal(double amount) {
+        if (amount <= this.balance) {
+            this.balance -= amount;
+        } else {
+            System.out.println("Insufficient funds");
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Account Holder: " + this.accountHolderName + ", Balance: " + this.balance;
+    }
+
+
+
+
 }
--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,2 +1,59 @@
-package org.example;public class Main {
+package org.example;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Main {
+    public static ArrayList<BankAccount> bankAccounts = new ArrayList<>();
+
+    public static void mainMenu(BankAccount account) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Hello " + account.getAccountHolderName() + "\nWelcome to the Main Menu, what would you " +
+                "like to do today?");
+        System.out.println("1: To check account balance\n2: To make a withdraw\n3: To make a deposit\n4: " +
+                "To make a transfer to another account");
+        int choice = scanner.nextInt();
+
+        switch (choice) {
+            case 1:
+                account.getBalance();
+                break;
+            case 2:
+                System.out.println("How much would you like to withdraw?");
+                double amount = scanner.nextDouble();
+                account.withdrawal(amount);
+                break;
+            case 3:
+                System.out.println("How much would you like to deposit?");
+                amount = scanner.nextDouble();
+                account.deposit(amount);
+                break;
+            case 4:
+                System.out.println("Please enter the account number you want to transfer to");
+                int accountNumber = scanner.nextInt();
+                boolean stop = false;
+                int i = 0;
+                while (!stop) {
+                    if (bankAccounts.get(i).getAccountNumber() == accountNumber){
+                        System.out.println("Please enter the amount you would like to transfer");
+                        double transferAmount = scanner.nextDouble();
+                        account.bankTransfer(bankAccounts.get(i) ,transferAmount);
+                        stop = true;
+                    } else {
+                        System.out.println("FINISH HERE");
+                    }
+
+                }
+                break;
+            case 5:
+                break;
+        }
+
+    }
+    public static void main(String[] args) {
+        bankAccounts.add(new BankAccount(100.0, "John Doe", 1));
+        bankAccounts.add(new BankAccount(200.0, "Jane Doe", 2));
+
+        mainMenu(bankAccounts.get(0));
+    }
 }
